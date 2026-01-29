371b74f18f25ddacaad630118f87aa56df25b581
Aruna Priyadarshana Bandara <arunapbandara45@gmail.com>
2026-01-29 22:23:12 +0530

    fix: swap couple and floral stem images based on user feedback

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,5 +1,5 @@
-import CoupleIllustration from "./1.png";
-import FloralStem from "./2.png";
+import CoupleIllustration from "./2.png";
+import FloralStem from "./1.png";
 import FloralSplash from "./3.png";
 import FloralBouquet from "./4.png";
 
